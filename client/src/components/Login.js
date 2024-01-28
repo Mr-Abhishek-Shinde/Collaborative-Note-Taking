faaeb72553d5a81dd8388f4a288ef0279e173988
Rutuja-tech2103 <rutujabs2103@gmail.com>
2024-01-28 15:58:59 +0530

    added components for login_signup_otp

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import styles from "../styles/Login.module.css";
+import loginimg from "../image/loginimg.png"
 
 const Login = () => {
   return (
-    <div>Login form here</div>
-  )
-}
+    <div className={styles["container"]}>
+      {/* <input type="checkbox" id="flip" /> */}
+      <div className={styles["cover"]}>
+        <div className={styles["front"]}>
+          <img src={loginimg} alt="not found" />
+          
+        </div>
+      </div>
+      <div className={styles["forms"]}>
+        <div className={styles["form-content"]}>
+          <div className={styles["login-form"]}>
+            <div className={styles["title"]}>Login</div>
+            <form action="#">
+              <div className={styles["input-boxes"]}>
+                <div className={styles["input-box"]}>
+                  <i className="fas fa-envelope"></i>
+                  <input type="text" placeholder="Enter your email" required />
+                </div>
+                <div className={styles["input-box"]}>
+                  <i className="fas fa-lock"></i>
+                  <input type="password" placeholder="Enter your password" required />
+                </div>
+                {/* <div className={styles["text"]}><a href="#">Forgot password?</a></div> */}
+                <div className={`${styles["button"]} ${styles["input-box"]}`}>
+                  <input type="submit" value="Submit" />
+                </div>
+                
+              </div>
+            </form>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
