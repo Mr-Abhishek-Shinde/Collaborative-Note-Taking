3bafb7776c77cf1981700fde3fc03537f0cf70ad
Abhishek <abhishinde889@gmail.com>
2024-02-01 11:00:22 +0530

    added frontend logic for authentication

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/Login.module.css";
 import loginimg from "../image/loginimg.png";
 
+import { useLogin } from "../hooks/useLogin";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { login, error, isLoading } = useLogin();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    await login(email, password);
+  };
+
   return (
     <div className={styles["container"]}>
       <div className={styles["cover"]}>
@@ -17,23 +30,36 @@ const Login = () => {
             <form action="#">
               <div className={styles["input-boxes"]}>
                 <div className={styles["input-box"]}>
-                  <input type="text" placeholder="Enter your email" required />
+                  <input
+                    type="text"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
                 </div>
                 <div className={styles["input-box"]}>
                   <input
                     type="password"
                     placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                     required
                   />
                 </div>
-                <div className={`${styles["button"]} ${styles["input-box"]}`}>
-                  <input type="submit" value="Submit" />
-                </div>
+                <button
+                  onClick={handleLogin}
+                  className="button input-box"
+                  disabled={isLoading}
+                >
+                  Login
+                </button>
               </div>
             </form>
           </div>
         </div>
       </div>
+      {error && <div className="error">{error}</div>}
     </div>
   );
 };
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,87 +1,76 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useSignup } from "../hooks/useSignup";
 import styles from "../styles/Signup.module.css";
-import OtpVerify from "./OtpVerify";
 
 const Signup = () => {
-  const [isOtpVisible, setIsOtpVisible] = useState(false);
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [name, setName] = useState('');
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const { signup, error, isLoading } = useSignup();
 
-  const handleSubmit = (e) => {
+  const handleSignup = async (e) => {
     e.preventDefault();
 
-    // validation logic here
-
-    setIsOtpVisible(true);
+    await signup(name, username, email, password);
   };
 
   return (
     <div className={styles.container}>
       <div className={styles.form}>
         <div className={styles.title}>Signup</div>
-        {!isOtpVisible ? (
-          <form onSubmit={handleSubmit}>
-            <div className={styles.inputBox}>
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
-                id="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-              />
-            </div>
-            <div className={styles.inputBox}>
-              <label htmlFor="username">Username</label>
-              <input
-                type="text"
-                id="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-              />
-            </div>
-            <div className={styles.inputBox}>
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
-            <div className={styles.inputBox}>
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </div>
-            <div className={styles.inputBox}>
-              <label htmlFor="confirmPassword">Confirm Password</label>
-              <input
-                type="password"
-                id="confirmPassword"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                required
-              />
-            </div>
-            <div className={styles.button}>
-              <button type="submit">Proceed</button>
-            </div>
-          </form>
-        ) : (
-          <OtpVerify />
-        )}
+            <form>
+              <div className={styles.inputBox}>
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  id="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </div>
+              <div className={styles.inputBox}>
+                <label htmlFor="username">Username</label>
+                <input
+                  type="text"
+                  id="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
+              </div>
+              <div className={styles.inputBox}>
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className={styles.inputBox}>
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+
+              <button
+                onClick={handleSignup}
+                className={styles.button}
+                disabled={isLoading}
+              >
+                Sign Up
+              </button>
+            </form>
+            {error && <div className="error">{error}</div>}
       </div>
     </div>
   );
