531b8e7d1f8131f055f907e0e4cee4507b3effcd
Rashi Sinha <rashis1146@gmail.com>
2024-02-22 06:15:36 +0530

    Dashobaord basics

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,8 +1,76 @@
 import React from 'react'
+import styles from '../styles/Dashboard.module.css';
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <>
+    <div className={styles.main}>
+ 
+  <ul className={styles.cards}>
+    <li className={styles.cards_item}>
+      <div className={styles.card}>
+        <div className={styles.card_img}><img src=""/></div>
+        <div className={styles.card_content}>
+          <h2 className={styles.card_title}>Card Grid Layout</h2>
+          <p className={styles.card_text}>Demo of pixel perfect pure CSS simple responsive card grid layout</p>
+          <button className={styles.btn}>Read More</button>
+        </div>
+      </div>
+    </li>
+    <li className={styles.cards_item}>
+      <div className={styles.card}>
+        <div className={styles.card_img}><img src=""/></div>
+        <div className={styles.card_content}>
+          <h2 className={styles.card_title}>Card Grid Layout</h2>
+          <p className={styles.card_text}>Demo of pixel perfect pure CSS simple responsive card grid layout</p>
+          <button className={styles.btn}>Read More</button>
+        </div>
+      </div>
+    </li>
+    <li className={styles.cards_item}>
+      <div className={styles.card}>
+        <div className={styles.card_img}><img src=""/></div>
+        <div className={styles.card_content}>
+          <h2 className={styles.card_title}>Card Grid Layout</h2>
+          <p className={styles.card_text}>Demo of pixel perfect pure CSS simple responsive card grid layout</p>
+          <button className={styles.btn}>Read More</button>
+        </div>
+      </div>
+    </li>
+    <li className={styles.cards_item}>
+      <div className={styles.card}>
+        <div className={styles.card_img}><img src=""/></div>
+        <div className={styles.card_content}>
+          <h2 className={styles.card_title}>Card Grid Layout</h2>
+          <p className={styles.card_text}>Demo of pixel perfect pure CSS simple responsive card grid layout</p>
+          <button className={styles.btn}>Read More</button>
+        </div>
+      </div>
+    </li>
+    <li className={styles.cards_item}>
+      <div className={styles.card}>
+        <div className={styles.card_img}><img src=""/></div>
+        <div className={styles.card_content}>
+          <h2 className={styles.card_title}>Card Grid Layout</h2>
+          <p className={styles.card_text}>Demo of pixel perfect pure CSS simple responsive card grid layout</p>
+          <button className={styles.btn}>Read More</button>
+        </div>
+      </div>
+    </li>
+    <li className={styles.cards_item}>
+      <div className={styles.card}>
+        <div className={styles.card_img}><img src=""/></div>
+        <div className={styles.card_content}>
+          <h2 className={styles.card_title}>Card Grid Layout</h2>
+          <p className={styles.card_text}>Demo of pixel perfect pure CSS simple responsive card grid layout</p>
+          <button className={styles.btn}>Read More</button>
+        </div>
+      </div>
+    </li>
+  </ul>
+</div>
+    </>
+    
   )
 }
 
