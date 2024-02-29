c184865db82952976d9ce5b6e83cad7ddbd7c21b
Rashi Sinha <rashis1146@gmail.com>
2024-02-29 09:58:17 +0530

    Dashboard Done

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,16 +2,48 @@ import React from 'react'
 import styles from '../styles/Dashboard.module.css';
 
 const Dashboard = () => {
+
+  
+  const openNav = () => {
+    const sidenav = document.getElementById("mainSideNav");
+    if (sidenav) {
+      sidenav.style.width = "250px";
+    }
+    else{
+      alert("Not found");
+    }
+  };
+
+
+const closeNav = () => {
+  const sidenav = document.getElementById("mainSideNav");
+  if (sidenav) {
+    sidenav.style.width = "0";
+  }
+};
+
   return (
     <>
     <div className={styles.main}>
- 
+        <div id="mainSideNav" className={styles.sidenav}>
+          <a href="javascript:void(0)" className={styles.closebtn} onClick={closeNav}>&times;</a>
+          <a href="#">About</a>
+          <a href="#">Services</a>
+          <a href="#">Clients</a>
+          <a href="#">Contact</a>
+        </div>
+        <span id={styles.lines}
+          
+          onClick={openNav}
+        >
+          &#9776;
+        </span>
   <ul className={styles.cards}>
     <li className={styles.cards_item}>
       <div className={styles.card}>
         <div className={styles.card_img}><img src=""/></div>
         <div className={styles.card_content}>
-          <h2 className={styles.card_title}>Card Grid Layout</h2>
+          <h2 className={styles.card_title}>Notes</h2>
           <p className={styles.card_text}>Demo of pixel perfect pure CSS simple responsive card grid layout</p>
           <button className={styles.btn}>Read More</button>
         </div>
@@ -21,7 +53,7 @@ const Dashboard = () => {
       <div className={styles.card}>
         <div className={styles.card_img}><img src=""/></div>
         <div className={styles.card_content}>
-          <h2 className={styles.card_title}>Card Grid Layout</h2>
+          <h2 className={styles.card_title}>To-do List</h2>
           <p className={styles.card_text}>Demo of pixel perfect pure CSS simple responsive card grid layout</p>
           <button className={styles.btn}>Read More</button>
         </div>
@@ -31,42 +63,13 @@ const Dashboard = () => {
       <div className={styles.card}>
         <div className={styles.card_img}><img src=""/></div>
         <div className={styles.card_content}>
-          <h2 className={styles.card_title}>Card Grid Layout</h2>
+          <h2 className={styles.card_title}>Mind-Map</h2>
           <p className={styles.card_text}>Demo of pixel perfect pure CSS simple responsive card grid layout</p>
           <button className={styles.btn}>Read More</button>
         </div>
       </div>
     </li>
-    <li className={styles.cards_item}>
-      <div className={styles.card}>
-        <div className={styles.card_img}><img src=""/></div>
-        <div className={styles.card_content}>
-          <h2 className={styles.card_title}>Card Grid Layout</h2>
-          <p className={styles.card_text}>Demo of pixel perfect pure CSS simple responsive card grid layout</p>
-          <button className={styles.btn}>Read More</button>
-        </div>
-      </div>
-    </li>
-    <li className={styles.cards_item}>
-      <div className={styles.card}>
-        <div className={styles.card_img}><img src=""/></div>
-        <div className={styles.card_content}>
-          <h2 className={styles.card_title}>Card Grid Layout</h2>
-          <p className={styles.card_text}>Demo of pixel perfect pure CSS simple responsive card grid layout</p>
-          <button className={styles.btn}>Read More</button>
-        </div>
-      </div>
-    </li>
-    <li className={styles.cards_item}>
-      <div className={styles.card}>
-        <div className={styles.card_img}><img src=""/></div>
-        <div className={styles.card_content}>
-          <h2 className={styles.card_title}>Card Grid Layout</h2>
-          <p className={styles.card_text}>Demo of pixel perfect pure CSS simple responsive card grid layout</p>
-          <button className={styles.btn}>Read More</button>
-        </div>
-      </div>
-    </li>
+    
   </ul>
 </div>
     </>
