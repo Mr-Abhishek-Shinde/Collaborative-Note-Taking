7b87639be235435b244bae730cb8f4b3465016f9
Abhishek <abhishinde889@gmail.com>
2024-04-08 00:49:06 +0530

    changed todo schema and added todo routes

--- client/src/components/TodoEditor.js
+++ client/src/components/TodoEditor.js
@@ -1,24 +1,26 @@
 import React, { useState } from "react";
 import styles from "../styles/TodoEditor.module.css";
 
-
-const  TodoEditor = ({text, updateMode, deleteToDo}) => {
-    
-    return (
-        <div className={styles.mainTodoContainer}>
-            <div className={styles.todo}>
-                <div className={styles.todoText}>
-                    {text}
-                </div>
-                <div className={styles.todoIcons}>
-                    <i className={styles.todoIcon} class="fa-regular fa-pen-to-square" onClick={updateMode}></i>
-                    <i className={styles.todoIcon} class="fa-regular fa-trash-can" onClick={deleteToDo}></i>
-                </div>
-            </div>
-
+const TodoEditor = ({ text, updateMode, deleteToDo }) => {
+  return (
+    <div className={styles.mainTodoContainer}>
+      <div className={styles.todo}>
+        <div className={styles.todoText}>{text}</div>
+        <div className={styles.todoIcons}>
+          <i
+            className={styles.todoIcon}
+            class="fa-regular fa-pen-to-square"
+            onClick={updateMode}
+          ></i>
+          <i
+            className={styles.todoIcon}
+            class="fa-regular fa-trash-can"
+            onClick={deleteToDo}
+          ></i>
         </div>
-        
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default TodoEditor
+export default TodoEditor;
