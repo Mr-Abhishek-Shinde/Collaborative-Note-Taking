e3b8773ba754afe88a51433c95a30d4f03cd44b9
Rutuja-tech2103 <rutujabs2103@gmail.com>
2024-04-15 00:24:09 +0530

    slider images changed

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -1,28 +1,22 @@
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import imgCa1 from '../image/ssNoteHome.png';
-import imgCa2 from '../image/ssAccess.png';
+import imgCa1 from '../image/caro01.jpeg';
+import imgCa2 from '../image/caro02.jpeg';
+import imgCa3 from '../image/caro03.jpeg';
 
 function CarouselFadeExample() {
   return (
     <Carousel fade prevIcon={<i className="fa-solid fa-angle-left" style={{ color: 'black', fontSize: '30px' }}></i>} nextIcon={<i className="fa-solid fa-angle-right" style={{ color: 'black', fontSize: '30px' }}></i>}>
       <Carousel.Item>
         <img src={imgCa1} alt="First slide" className="img-fluid" style={{ maxWidth: '90%', maxHeight: '100%' }} />
-        <Carousel.Caption>
-          <h3>First slide label</h3>  
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img src={imgCa2} alt="Second slide" className="img-fluid" style={{ maxWidth: '90%', maxHeight: '100%' }} />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-        </Carousel.Caption>
+
       </Carousel.Item>
       <Carousel.Item>
-        <img src={imgCa1} alt="Third slide" className="img-fluid" style={{ maxWidth: '90%', maxHeight: '100%' }} />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-        </Carousel.Caption>
+        <img src={imgCa3} alt="Third slide" className="img-fluid" style={{ maxWidth: '90%', maxHeight: '100%' }} />
+
       </Carousel.Item>
     </Carousel>
   );
